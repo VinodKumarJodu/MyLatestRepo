44e008bd950190245153c276de19bcba1a8131e2
VinodKumarJodu <vinodkumarjodu@gmail.com>
2021-12-17 20:34:18 +0530

    Initial Commit

---
+++ model.py
@@ -0,0 +1,16 @@
+import joblib
+import xgboost
+import os
+import numpy as np
+
+curr_path = os.path.dirname(os.path.realpath(__file__))
+print('Current Path: ', curr_path)
+xgb = joblib.load(r'E:\AgriN2OEmission\boost_final.pkl.compressed')
+
+
+def predict(attributes: np.array):
+    pred = xgb.predict(attributes)
+
+    print('Flux Value Predicted')
+
+    return pred[0]
